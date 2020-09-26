1b60c60e05a05930e103858bc078bdcd151b108b
Mohammadreza Molaei <mrmolaei.ir@gmail.com>
2020-09-26 08:56:46 +0330

    Add options to control group cells in categories carousel

--- js/main.js
+++ js/main.js
@@ -1,18 +1,24 @@
 if (document.querySelectorAll(".js-categories-list").length) {
-    var initIndex = 0;
+    let initIndex = 0;
      document.querySelectorAll('.js-categories-list li').forEach(function(item, index) {
         if (item.classList.contains('current-cat')) {
             initIndex = index;
         }
     });
-    console.log(initIndex);
-    var categoriesCarousel = new Flickity( '.js-categories-list', {
-        initialIndex: initIndex,
-        freeScroll: true,
-        contain: true,
-        pageDots: false,
-        groupCells: false,
-        cellAlign: 'left'
-    });
+
+     const categoriesCarouselOptions = {
+         initialIndex: initIndex,
+         freeScroll: true,
+         contain: true,
+         pageDots: false,
+         groupCells: true,
+         cellAlign: 'left'
+     };
+
+     if (window.matchMedia("(max-width: 720px)")) {
+         categoriesCarouselOptions.groupCells = false;
+     }
+
+    const categoriesCarousel = new Flickity( '.js-categories-list', categoriesCarouselOptions);
 }
 
