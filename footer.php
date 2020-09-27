92059e2496f49eeaf7c2f1073761f25baf8c5331
Mohammadreza Molaei <mrmolaei.ir@gmail.com>
2020-09-27 10:03:30 +0330

    Implement footer

--- footer.php
+++ footer.php
@@ -11,21 +11,31 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="u-default-max-width site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'wp_meliora' ) ); ?>">
+<footer id="colophon" class="c-footer site-footer">
+    <div class="c-footer__top">
+        <div class="c-footer__top__back-to-top">
+            <a class="c-footer__top__back-to-top__link" href="#masthead">
+                <span class="dashicons dashicons-arrow-up-alt2"></span>
+            </a>
+        </div>
+    </div>
+    <div class="c-footer__bottom site-info">
+        <div class="u-default-max-width">
+            <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'wp_meliora' ) ); ?>">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
 				printf( esc_html__( 'Proudly powered by %s', 'wp_meliora' ), 'WordPress' );
 				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'wp_meliora' ), 'wp_meliora', '<a href="https://vitathemes.com">VitaThemes</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+            </a>
+            <span class="sep"> | </span>
+			<?php
+			/* translators: 1: Theme name, 2: Theme author. */
+			printf( esc_html__( 'Theme: %1$s by %2$s.', 'wp_meliora' ), 'WP-Meliora', '<a href="https://vitathemes.com">VitaThemes</a>' );
+			?>
+        </div><!-- .site-info -->
+    </div>
+</footer><!-- #colophon -->
+
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
