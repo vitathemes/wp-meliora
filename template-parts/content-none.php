beb14399097ed99f97de9c85b45658d12b94acd4
Mohammadreza Molaei <mrmolaei.ir@gmail.com>
2020-10-04 13:18:14 +0330

    Fix header issue on tablet and small screens

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -9,7 +9,7 @@
 
 ?>
 
-<section class="no-results not-found">
+<section class="no-results not-found c-post c-post--single">
 	<header class="page-header">
 		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'wp-meliora' ); ?></h1>
 	</header><!-- .page-header -->
