5339b468303d49e65f73e9444f1396f10a20e498
Mohammadreza Molaei <mrmolaei.ir@gmail.com>
2020-12-21 16:55:31 +0330

    Remove page title from Blocks page template

--- page-templates/blocks-template.php
+++ page-templates/blocks-template.php
@@ -8,9 +8,45 @@ get_header();
 		<?php
 		while ( have_posts() ) :
 			the_post();
+		?>
+            <article id="post-<?php the_ID(); ?>" <?php post_class('c-page'); ?>>
+                <div class="entry-content">
+					<?php
+					the_content();
 
-			get_template_part( 'template-parts/content', 'page' );
+					wp_link_pages(
+						array(
+							'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'wp-meliora' ),
+							'after'  => '</div>',
+						)
+					);
+					?>
+                </div><!-- .entry-content -->
 
+				<?php if ( get_edit_post_link() ) : ?>
+                    <footer class="entry-footer">
+						<?php
+						edit_post_link(
+							sprintf(
+								wp_kses(
+								/* translators: %s: Name of current post. Only visible to screen readers */
+									__( 'Edit <span class="screen-reader-text">%s</span>', 'wp-meliora' ),
+									array(
+										'span' => array(
+											'class' => array(),
+										),
+									)
+								),
+								wp_kses_post( get_the_title() )
+							),
+							'<span class="edit-link">',
+							'</span>'
+						);
+						?>
+                    </footer><!-- .entry-footer -->
+				<?php endif; ?>
+            </article><!-- #post-<?php the_ID(); ?> -->
+        <?php
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
 				comments_template();
