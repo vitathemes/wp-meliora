7acd914416fa20a8d65754e56d636db5d94822b3
Mohammadreza Molaei <mrmolaei.ir@gmail.com>
2021-12-07 21:35:55 +0330

    Add OCDI

--- functions.php
+++ functions.php
@@ -224,3 +224,55 @@ require get_template_directory() . '/classes/class_wp_meliora_walker_comment.php
  */
 require_once get_template_directory() . '/inc/tgmpa/class-tgm-plugin-activation.php';
 require_once get_template_directory() . '/inc/tgmpa-config.php';
+
+
+// OCDI - Demo Importer Config
+function wp_meliora_register_plugins( $plugins ) {
+	$theme_plugins = [
+		[
+			'name'     => __( 'Kirki Customizer Framework', 'wp-indigo' ),
+			'slug'     => 'kirki',
+			'required' => false,
+		]
+	];
+
+	return array_merge( $plugins, $theme_plugins );
+}
+add_filter( 'ocdi/register_plugins', 'wp_meliora_register_plugins' );
+
+
+function wp_meliora_import_files() {
+	return [
+		[
+			'import_file_name'           => 'Default Demo',
+			'import_file_url'            => 'https://demo.vitathemes.com/ocdi/meliora/meliora.xml',
+			'import_widget_file_url'     => 'https://demo.vitathemes.com/ocdi/meliora/meliora-widgets.wie',
+			'import_customizer_file_url' => 'https://demo.vitathemes.com/ocdi/meliora/meliora-customizer.dat',
+			'import_preview_image_url'   => 'https://demo.vitathemes.com/ocdi/meliora/screenshot.png',
+			'preview_url'                => 'https://demo.vitathemes.com/meliora',
+		],
+	];
+}
+add_filter( 'ocdi/import_files', 'wp_meliora_import_files' );
+
+function wp_meliora_after_import_setup() {
+	// Assign menus to their locations.
+	$primary_menu = get_term_by( 'name', 'Primary', 'nav_menu' );
+	$slider_menu = get_term_by( 'name', 'Slider-Menu', 'nav_menu' );
+	set_theme_mod( 'nav_menu_locations', [
+			'menu-1' => $primary_menu->term_id,
+			'menu-2' => $slider_menu->term_id,
+		]
+	);
+
+	// Unset Logo
+	set_theme_mod( 'custom_logo', 0);
+
+	// Social Networks
+	set_theme_mod( 'facebook', '#' );
+	set_theme_mod( 'twitter', 'https://twitter.com/veronalabs' );
+	set_theme_mod( 'instagram', 'https://www.instagram.com/veronalabs/' );
+	set_theme_mod( 'linkedin', 'https://www.linkedin.com/company/veronalabs/' );
+}
+add_action( 'ocdi/after_import', 'wp_meliora_after_import_setup' );
+
